35bf09c466ee3a28685a026f16a5cb1f6ed93a2a
LerroG <ssakoks@mail.ru>
2025-07-10 17:54:09 +0500

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { CoreModule } from './core/core.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(CoreModule)
+	app.enableCors({
+		origin: process.env.CLIENT_URL ?? 'http://localhost:3000',
+		credentials: true
+	})
+	app.setGlobalPrefix('api')
 	await app.listen(process.env.PORT ?? 3000)
 }
 void bootstrap()
